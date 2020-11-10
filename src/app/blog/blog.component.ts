9781a692471a4832104026b501fb4093e9ccb013
DESKTOP-CD0NI2Q\juanm <juanmluces@gmail.com>
2020-11-10 19:43:25 +0100

    primer commit en blog.component.ts

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../interfaces/post';
+import { DatosPostsService } from '../services/datos-posts.service';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  arrayMostrado: Post[];
+
+  constructor(private datosPosts: DatosPostsService) { }
 
   ngOnInit(): void {
+
+    this.datosPosts.getAllPosts()
+      .then(response => {
+        this.arrayMostrado = response;
+        console.log(this.arrayMostrado);
+
+      })
+      .catch(error => console.log(error));
+  }
+
+  async filtrarCategoria(pCategoria) {
+    this.arrayMostrado = await this.datosPosts.getPostByCategory(pCategoria)
   }
 
 }
