b06f62435cc3e3c7f752e55609a2b18fc2aa7c69
DESKTOP-CD0NI2Q\juanm <juanmluces@gmail.com>
2020-11-10 18:32:03 +0100

    creacion componente NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
-    FormularioComponent
+    FormularioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
